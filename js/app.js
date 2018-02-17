240065ff84f827ebf343df1b19492f84ff6402a9
Huw Fernie <HuwFernie@gmail.com>
2018-02-17 13:03:10 +0000

    drag and drop OK into the first square only

--- js/app.js
+++ js/app.js
@@ -8,14 +8,19 @@ $(() => {
     allDragObjects[i].addEventListener('dragstart', handleDragStart, false);
   }
 
-  function handleDragStart(){
+  function handleDragStart(e){
     console.log('dragstart');
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
   }
 
   const allDragTargets = document.getElementsByClassName('square');
   for(let i=0; i<allDragTargets.length; i++){
     allDragTargets[i].addEventListener('dragenter', handleDragEnter, false);
+    // allDragTargets[i].addEventListener('dragover', handleDragOver, false);
     allDragTargets[i].addEventListener('dragleave', handleDragLeave, false);
+    allDragTargets[i].addEventListener('drop', handleDrop, false);
+    allDragTargets[i].addEventListener('dragend', handleDragEnd, false);
   }
 
   function handleDragEnter(e) {
@@ -25,6 +30,22 @@ $(() => {
 
   function handleDragLeave(e) {
     e.target.classList.remove('over');  // this / e.target is previous target element.
+  }
+
+  function handleDrop(e) {
+    // this / e.target is current target element.
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+    this.innerHTML = e.dataTransfer.getData('text/html');
+    handleDragEnd();
+    return false;
+  }
+
+  function handleDragEnd() {
+    [].forEach.call(allDragTargets, function (col) {
+      col.classList.remove('over');
+    });
   }
 
 });
