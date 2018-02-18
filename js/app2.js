003ec1fe5fcebfa803224cf0387e21437abdc9b6
Huw Fernie <HuwFernie@gmail.com>
2018-02-18 11:30:12 +0000

    added code to app2 for the reset button

--- js/app2.js
+++ js/app2.js
@@ -6,18 +6,22 @@ $(() => {
   const randomGame = document.getElementById('randomGame');
   randomGame.addEventListener('click', ()=>{
     random();
+    setResetButton();
   });
-
 });
 
 function random(){
   console.log('randomGame');
+  const i = 0; //change this for a random number generator later on.
+  setBoard(i);
+  return;
+}
+
+function setBoard(i){
   // RandomGames is an array of strings, each 9 numbers is a box, the commas are stripped out at a later point.
   const randomGames = ['530600098,070195000,000000060,800400700,060803020,003001006,060000000,000419080,280005079','008000200,300400600,402300591,619000000,004090200,000000915,143,006902,007003005,009000800'];
 
-  const i = 0; //change this for a random number generator later on.
   let game = randomGames[i];
-
   game = game.replace(/,/g,'');
   console.log('should be 81: ',game.length);
   game = game.split('');
@@ -28,6 +32,20 @@ function random(){
     if(elem>0) {
       squares[index].innerHTML = elem;
       squares[index].classList.add('fixed');
+    } else {
+      squares[index].innerHTML = '';
+      squares[index].classList.remove('fixedUser');
     }
+  });
+  return;
+}
+
+function setResetButton() {
+  document.getElementById('resetGame').addEventListener('click', ()=>{
+    const squares = document.getElementsByClassName('fixedUser');
+    [].forEach.call(squares, (square)=>{
+      square.innerHTML = '';
+      square.classList.remove('fixedUser');
+    });
   });
 }
